9c7f21dc98b08798bcf24ad193d231d0de72d514
Daniel Tovar <daniel.tovar@cargamos.com>
2021-04-22 20:50:50 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<div class="background"></div>',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'technical-test';
+}
